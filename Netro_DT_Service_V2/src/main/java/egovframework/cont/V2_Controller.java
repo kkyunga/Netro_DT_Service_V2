3056491e165bdc22ffaa7c4198400f34613ba160
KIM kyung a <119964637+kkyunga@users.noreply.github.com>
2025-01-09 10:05:26 +0900

    init

---
+++ Netro_DT_Service_V2/src/main/java/egovframework/cont/V2_Controller.java
@@ -0,0 +1,13 @@
+package egovframework.cont;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class V2_Controller {
+	
+	@GetMapping("/test")
+	public String getTestString() {
+		return "Hello World!";
+	}
+}
